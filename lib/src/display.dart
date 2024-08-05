cf6421e3b87b6949c775c8c5bc0f9fcc43454cdd
Igor Belous <belous@schielicke-bau.de>
2024-08-05 16:48:34 +0200

    WIP

--- lib/src/display.dart
+++ lib/src/display.dart
@@ -197,7 +197,7 @@ class Display {
       case Unit.kwWeek:
         return diff(firstDateTime, secondDateTime, Unit.kwWeek, false);
       case Unit.halfMonth:
-        return (diffAbsolute(firstDateTime, secondDateTime, Unit.month, false) * 2).ceil();
+        return (diff(firstDateTime, secondDateTime, Unit.halfMonth, false) * 2).round();
       case Unit.month:
         int yearAddition = 12 * (firstDateTime.year - secondDateTime.year);
         return firstDateTime.month - secondDateTime.month + 1 + yearAddition;
